618045c85c6a33a48744b14d1dd9b91e33c261d3
Selen Parlar <Pselen@users.noreply.github.com>
2018-11-24 20:13:51 +0300

    added evaluation

--- CMPE561_Assignment1.py
+++ CMPE561_Assignment1.py
@@ -3,6 +3,10 @@
 Created on Tue Nov 20 00:38:55 2018
 
 @author: Bilal
+laplace
+bigram
+surface form
+unk = 1 
 """
 #%%
 import numpy as np
@@ -114,83 +118,112 @@ transition_probabilities[np.isnan(transition_probabilities)] = 0
 observation_likelihoods = observation_counts / np.sum(observation_counts, axis=1).reshape(-1,1)
 
 #%%
-# viterbi
-input_sentence = [i[0] for i in sentences_test[1][1:-1]] # example observation input
-
-input_length = len(input_sentence) + 2
-number_of_states  = number_of_pos
-
-# intilize table, all zero 0
-viterbi_table = [[[0.0,0] for column in range(input_length)] for row in range(number_of_states)]
-viterbi_table[pos_dictionary['<s>']][0][0] = 1
-
-# states without start and end
-states = {k:v for k,v in pos_dictionary.items() if k != '<s>' and k != '</s>'}
-
-# fill the first column
-for state,state_idx in states.items():
-    word_idx = word_dictionary.get(input_sentence[0], -1)
-    start_state_idx = pos_dictionary['<s>']
+def viterbi(input_sentence, transition_probabilities, observation_likelihoods, number_of_pos, word_dictionary, pos_dictionary):
+    # viterbi
+#    input_sentence = [i[0] for i in sentences_test[1][1:-1]] # example observation input
     
-    transition_p = transition_probabilities[start_state_idx][state_idx]
-    # CHECK UNKOWN WORD
-    # if word is not in the dictionary just ignore it by assigning it to 1
-    obsservation_p = 1
-    if word_idx != -1:
-        obsservation_p = observation_likelihoods[state_idx][word_idx]
+    input_length = len(input_sentence) + 2
+    number_of_states  = number_of_pos
     
-    viterbi_table[state_idx][1][0] = transition_p * obsservation_p
-    viterbi_table[state_idx][1][1] = start_state_idx
-
-# fill all table, i=pre_state , j=state. zip for observation idx
-for word,v_t in zip(input_sentence[1:], range(2,len(input_sentence) + 1)):
-    word_idx = word_dictionary .get(word, -1)
+    # intilize table, all zero 0
+    viterbi_table = [[[0.0,0] for column in range(input_length)] for row in range(number_of_states)]
+    viterbi_table[pos_dictionary['<s>']][0][0] = 1
+    
+    # states without start and end
+    states = {k:v for k,v in pos_dictionary.items() if k != '<s>' and k != '</s>'}
+    
+    # fill the first column
     for state,state_idx in states.items():
-        list_to_get_max = []
-        for pre_state,pre_state_idx in states.items():
-            # p(state | pre_state) a_ij
-            prob = transition_probabilities[pre_state_idx][state_idx]
-            # v_t-1(i)
-            v = viterbi_table[pre_state_idx][v_t-1][0]
-            # v_t-1(i) * a_ij
-            list_to_get_max.append((prob * v, pre_state_idx))
-            
-        # max( v_t-1(i) * a_ij) for i to N
-        value, pointer = max(list_to_get_max)
+        word_idx = word_dictionary.get(input_sentence[0], -1)
+        start_state_idx = pos_dictionary['<s>']
         
+        transition_p = transition_probabilities[start_state_idx][state_idx]
         # CHECK UNKOWN WORD
+        # if word is not in the dictionary just ignore it by assigning it to 1
         obsservation_p = 1
         if word_idx != -1:
             obsservation_p = observation_likelihoods[state_idx][word_idx]
         
-        # v_t = max( v_t-1(i) * a_ij) for i to N * b_j(o_t)
-        viterbi_table[state_idx][v_t][0] = value * obsservation_p
-        viterbi_table[state_idx][v_t][1] = pointer
-        
-# fill the last column, assume observation p( </s> | </s> ) = 1
-list_to_get_max = []
-end_state_idx = pos_dictionary['</s>']
-for pre_state,pre_state_idx in states.items():
-    # p(state | pre_state) a_ij
-    prob = transition_probabilities[pre_state_idx][end_state_idx]
-    # v_t-1(i)
-    v = viterbi_table[pre_state_idx][-2][0]
-    # v_t-1(i) * a_ij
-    list_to_get_max.append((prob * v, pre_state_idx))
-# max( v_t-1(i) * a_ij) for i to N
-value, pointer = max(list_to_get_max)
-# v_t = max( v_t-1(i) * a_ij) for i to N * b_j(o_t)
-viterbi_table[end_state_idx][-1][0] = value 
-viterbi_table[end_state_idx][-1][1] = pointer
-      
-
-# backtrack, extract path
-pointer = viterbi_table[end_state_idx][-1][1] 
-state_order = [pointer]
-for i in reversed(range(2,input_length - 1)):
-    pointer = viterbi_table[pointer][i][1]
-    state_order.append(pointer)
-pos_dictionary_rev = {v: k for k, v in pos_dictionary.items()}
-output = [pos_dictionary_rev[i] for i in reversed(state_order)]
-
+        viterbi_table[state_idx][1][0] = transition_p * obsservation_p
+        viterbi_table[state_idx][1][1] = start_state_idx
+    
+    # fill all table, i=pre_state , j=state. zip for observation idx
+    for word,v_t in zip(input_sentence[1:], range(2,len(input_sentence) + 1)):
+        word_idx = word_dictionary.get(word, -1)
+        for state,state_idx in states.items():
+            list_to_get_max = []
+            for pre_state,pre_state_idx in states.items():
+                # p(state | pre_state) a_ij
+                prob = transition_probabilities[pre_state_idx][state_idx]
+                # v_t-1(i)
+                v = viterbi_table[pre_state_idx][v_t-1][0]
+                # v_t-1(i) * a_ij
+                list_to_get_max.append((prob * v, pre_state_idx))
+                
+            # max( v_t-1(i) * a_ij) for i to N
+            value, pointer = max(list_to_get_max)
+            
+            # CHECK UNKOWN WORD
+            obsservation_p = 1
+            if word_idx != -1:
+                obsservation_p = observation_likelihoods[state_idx][word_idx]
+            # v_t = max( v_t-1(i) * a_ij) for i to N * b_j(o_t)
+            viterbi_table[state_idx][v_t][0] = value * obsservation_p
+            viterbi_table[state_idx][v_t][1] = pointer
+            
+    # fill the last column, assume observation p( </s> | </s> ) = 1
+    list_to_get_max = []
+    end_state_idx = pos_dictionary['</s>']
+    for pre_state,pre_state_idx in states.items():
+        # p(state | pre_state) a_ij
+        prob = transition_probabilities[pre_state_idx][end_state_idx]
+        # v_t-1(i)
+        v = viterbi_table[pre_state_idx][-2][0]
+        # v_t-1(i) * a_ij
+        list_to_get_max.append((prob * v, pre_state_idx))
+    # max( v_t-1(i) * a_ij) for i to N
+    value, pointer = max(list_to_get_max)
+    # v_t = max( v_t-1(i) * a_ij) for i to N * b_j(o_t)
+    viterbi_table[end_state_idx][-1][0] = value 
+    viterbi_table[end_state_idx][-1][1] = pointer
+          
+    
+    # backtrack, extract path
+    pointer = viterbi_table[end_state_idx][-1][1] 
+    state_order = [pointer]
+    for i in reversed(range(2,input_length - 1)):
+        pointer = viterbi_table[pointer][i][1]
+        state_order.append(pointer)
+    pos_dictionary_rev = {v: k for k, v in pos_dictionary.items()}
+    output = [pos_dictionary_rev[i] for i in reversed(state_order)]
+    return output
 #%%
+correctly_tagged_word = 0.0
+incorrectly_tagged_word = 0.0
+correctly_tagged_sentence = 0.0
+incorrectly_tagged_sentence = 0.0
+for sentence in sentences_test:
+    input_sentence = [i[0] for i in sentence[1:-1]] 
+    target_tags = [i[1] for i in sentence[1:-1]] 
+    
+    output = viterbi(input_sentence=input_sentence, transition_probabilities=transition_probabilities,
+                     observation_likelihoods=observation_likelihoods,
+                     number_of_pos=number_of_pos, word_dictionary=word_dictionary, pos_dictionary=pos_dictionary)
+    all_correct = True
+    for i,j in zip(output,target_tags):
+        if i==j:
+            correctly_tagged_word += 1
+        else:
+            incorrectly_tagged_word += 1
+            all_correct = False
+    
+    if all_correct:
+
+        correctly_tagged_sentence += 1
+    else:
+        incorrectly_tagged_sentence += 1
+#    break
+    
+        
+print(correctly_tagged_word / (correctly_tagged_word + incorrectly_tagged_word))
+print(correctly_tagged_sentence / (correctly_tagged_sentence + incorrectly_tagged_sentence))
