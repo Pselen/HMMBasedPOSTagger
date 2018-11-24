e8040e09454d6b7fac8493c52a9a97a367537b6d
Selen Parlar <Pselen@users.noreply.github.com>
2018-11-24 21:35:19 +0300

    added cross validation

--- CMPE561_Assignment1.py
+++ CMPE561_Assignment1.py
@@ -11,23 +11,13 @@ unk = 1
 #%%
 import numpy as np
 from sklearn.model_selection import train_test_split
-
+from sklearn.model_selection import KFold
 #%% 
+# Initializatins
 sentences = []
-word_dictionary = {}
-pos_dictionary = {}
-
-observation_counts = np.empty((0))
-transition_counts = np.empty((0))
-
-observation_likelihoods = np.empty((0))
-transition_probabilities = np.empty((0))
-
 #%% 
 with open(r"Project (Application 1) (MetuSabanci Treebank).conll", 'r', encoding='utf-8') as f:
     dataset = f.readlines()
-
-
 #%% 
 # Correct erronous tags of word satın
 for d in dataset:
@@ -37,9 +27,8 @@ for d in dataset:
             tag = 'Noun'
             new_d = d.split('\t')[0] + '\t' + d.split('\t')[1] + '\t' + d.split('\t')[2]  + '\t' + tag + '\t' + tag + '\t' + d.split('\t')[5]+ '\t' + d.split('\t')[6] + '\t' + d.split('\t')[7]  + '\t' + d.split('\t')[8] + '\t' + d.split('\t')[9] 
             dataset[dataset.index(d)] = new_d
-            
- #%%
-# extract sentences
+#%%
+# Extract sentences
 sentence = [('<s>','<s>')]
 for line in dataset:
     line_splited = line.split('\t')
@@ -56,67 +45,6 @@ for line in dataset:
         sentence.append(('</s>','</s>'))
         sentences.append(sentence)
         sentence = [('<s>','<s>')]
-
-#%%
-# TODO partion dataset into trainning and test
-sentences_train, sentences_test = train_test_split(sentences, test_size=0.1, random_state=1)
-
-#%%
-# extract all unique word&pos
-for sentence in sentences_train:
-    for word,pos in sentence:
-        if word != '<s>' and word != '</s>':
-            word_dictionary[word] = 0
-            pos_dictionary[pos] = 0
-
-# last two index
-# TODO maybe we should ignore </s> column (it may worsen the prediction though)
-pos_dictionary['<s>'] = 0
-pos_dictionary['</s>'] = 0       
-
-number_of_words = len(word_dictionary.keys())
-number_of_pos = len(pos_dictionary.keys())
-
-# assign them an unique index    
-for key,index in zip(word_dictionary.keys(), range(number_of_words)):
-    word_dictionary[key] = index
-
-for key,index in zip(pos_dictionary.keys(), range(number_of_pos)):
-    pos_dictionary[key] = index
-
-#%%
-# count observations
-observation_counts = np.zeros((number_of_pos-2, number_of_words), dtype=np.float32)
-for sentence in sentences_train:
-    for word,pos in sentence:
-            if word != '<s>' and word != '</s>':
-                observation_counts[pos_dictionary[pos]][word_dictionary[word]] += 1
-   
-# count transitions
-transition_counts = np.zeros((number_of_pos, number_of_pos), dtype=np.float32)
-for sentence in sentences_train:
-    for pos_0,pos_1 in zip(sentence,sentence[1:]):
-        transition_counts[pos_dictionary[pos_0[1]]][pos_dictionary[pos_1[1]]] += 1
-
-#%%
-# apply smoothing, be carefull with </s> 15th row(left as zero) and <s> 14th column(left as zero)
-# simple laplace smoothing
-# TODO: OTHER SMOOTHS
-transition_counts = transition_counts + 1 
-transition_counts[:,14] = 0
-transition_counts[15,:] = 0
-# TODO i am not sure, it should be applied to here
-# observation_counts = observation_counts + 1
-
-
-#%%
-# calculate probabilities
-# implementation detail(create seperate dictinary for each dimension,
-# makes more sense if elements got different values in each dim)
-transition_probabilities = transition_counts / np.sum(transition_counts, axis=1).reshape(-1,1)
-transition_probabilities[np.isnan(transition_probabilities)] = 0
-observation_likelihoods = observation_counts / np.sum(observation_counts, axis=1).reshape(-1,1)
-
 #%%
 def viterbi(input_sentence, transition_probabilities, observation_likelihoods, number_of_pos, word_dictionary, pos_dictionary):
     # viterbi
@@ -196,34 +124,103 @@ def viterbi(input_sentence, transition_probabilities, observation_likelihoods, n
         state_order.append(pointer)
     pos_dictionary_rev = {v: k for k, v in pos_dictionary.items()}
     output = [pos_dictionary_rev[i] for i in reversed(state_order)]
-    return output
+    return output        
 #%%
-correctly_tagged_word = 0.0
-incorrectly_tagged_word = 0.0
-correctly_tagged_sentence = 0.0
-incorrectly_tagged_sentence = 0.0
-for sentence in sentences_test:
-    input_sentence = [i[0] for i in sentence[1:-1]] 
-    target_tags = [i[1] for i in sentence[1:-1]] 
+accuracy_of_tagging_words = 0.0
+accuracy_of_tagging_sentences = 0.0
+a, b = [], []
+kf = KFold(n_splits=10,random_state=1, shuffle=False)
+for index_train, index_test in kf.split(sentences):
+    word_dictionary = {}
+    pos_dictionary = {}
     
-    output = viterbi(input_sentence=input_sentence, transition_probabilities=transition_probabilities,
-                     observation_likelihoods=observation_likelihoods,
-                     number_of_pos=number_of_pos, word_dictionary=word_dictionary, pos_dictionary=pos_dictionary)
-    all_correct = True
-    for i,j in zip(output,target_tags):
-        if i==j:
-            correctly_tagged_word += 1
-        else:
-            incorrectly_tagged_word += 1
-            all_correct = False
+    observation_counts = np.empty((0))
+    transition_counts = np.empty((0))
     
-    if all_correct:
-
-        correctly_tagged_sentence += 1
-    else:
-        incorrectly_tagged_sentence += 1
-#    break
+    observation_likelihoods = np.empty((0))
+    transition_probabilities = np.empty((0))
+    # extract all unique word&pos
     
+    sentences_train = [sentences[i] for i in index_train]
+    sentences_test = [sentences[i] for i in index_test]
+    
+    for sentence in sentences_train:
+        for word,pos in sentence:
+            if word != '<s>' and word != '</s>':
+                word_dictionary[word] = 0
+                pos_dictionary[pos] = 0
+    
+    # last two index
+    # TODO maybe we should ignore </s> column (it may worsen the prediction though)
+    pos_dictionary['<s>'] = 0
+    pos_dictionary['</s>'] = 0       
+    
+    number_of_words = len(word_dictionary.keys())
+    number_of_pos = len(pos_dictionary.keys())
+    
+    # assign them an unique index    
+    for key,index in zip(word_dictionary.keys(), range(number_of_words)):
+        word_dictionary[key] = index
+    
+    for key,index in zip(pos_dictionary.keys(), range(number_of_pos)):
+        pos_dictionary[key] = index
+    
+    # count observations
+    observation_counts = np.zeros((number_of_pos-2, number_of_words), dtype=np.float32)
+    for sentence in sentences_train:
+        for word,pos in sentence:
+                if word != '<s>' and word != '</s>':
+                    observation_counts[pos_dictionary[pos]][word_dictionary[word]] += 1
+       
+    # count transitions
+    transition_counts = np.zeros((number_of_pos, number_of_pos), dtype=np.float32)
+    for sentence in sentences_train:
+        for pos_0,pos_1 in zip(sentence,sentence[1:]):
+            transition_counts[pos_dictionary[pos_0[1]]][pos_dictionary[pos_1[1]]] += 1
+    
+    # apply smoothing, be carefull with </s> 15th row(left as zero) and <s> 14th column(left as zero)
+    # simple laplace smoothing
+    # TODO: OTHER SMOOTHS
+    transition_counts = transition_counts + 1 
+    transition_counts[:,14] = 0
+    transition_counts[15,:] = 0
+    # TODO i am not sure, it should be applied to here
+    # observation_counts = observation_counts + 1
+    
+    
+    # calculate probabilities
+    # implementation detail(create seperate dictinary for each dimension,
+    # makes more sense if elements got different values in each dim)
+    transition_probabilities = transition_counts / np.sum(transition_counts, axis=1).reshape(-1,1)
+    transition_probabilities[np.isnan(transition_probabilities)] = 0
+    observation_likelihoods = observation_counts / np.sum(observation_counts, axis=1).reshape(-1,1)
+    
+    correctly_tagged_word = 0.0
+    incorrectly_tagged_word = 0.0
+    correctly_tagged_sentence = 0.0
+    incorrectly_tagged_sentence = 0.0
+    for sentence in sentences_test:
+        input_sentence = [i[0] for i in sentence[1:-1]] 
+        target_tags = [i[1] for i in sentence[1:-1]] 
         
-print(correctly_tagged_word / (correctly_tagged_word + incorrectly_tagged_word))
-print(correctly_tagged_sentence / (correctly_tagged_sentence + incorrectly_tagged_sentence))
+        output = viterbi(input_sentence=input_sentence, transition_probabilities=transition_probabilities,
+                         observation_likelihoods=observation_likelihoods,
+                         number_of_pos=number_of_pos, word_dictionary=word_dictionary, pos_dictionary=pos_dictionary)
+        all_correct = True
+        for i,j in zip(output,target_tags):
+            if i==j:
+                correctly_tagged_word += 1
+            else:
+                incorrectly_tagged_word += 1
+                all_correct = False
+        
+        if all_correct:
+    
+            correctly_tagged_sentence += 1
+        else:
+            incorrectly_tagged_sentence += 1
+    accuracy_of_tagging_words += correctly_tagged_word / (correctly_tagged_word + incorrectly_tagged_word)
+    accuracy_of_tagging_sentences += correctly_tagged_sentence / (correctly_tagged_sentence + incorrectly_tagged_sentence)
+    a.append((correctly_tagged_word / (correctly_tagged_word + incorrectly_tagged_word),correctly_tagged_sentence / (correctly_tagged_sentence + incorrectly_tagged_sentence)))
+print(accuracy_of_tagging_words/10.0)
+print(accuracy_of_tagging_sentences/10.0)
